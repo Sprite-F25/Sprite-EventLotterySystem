caaafc96733063dbc0e303d1bc1fe41a5064fdce
NaimaU <naimaulusow42@gmail.com>
2025-10-28 21:06:09 -0600

    adding event list view

---
+++ app/src/main/java/com/example/sprite/screens/eventsList/EventsListFragment.java
@@ -0,0 +1,36 @@
+package com.example.sprite.screens.eventsList;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.sprite.R;
+
+public class EventsListFragment extends Fragment {
+
+    private EventsListViewModel mViewModel;
+
+    public static EventsListFragment newInstance() {
+        return new EventsListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_events_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(EventsListViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/sprite/screens/eventsList/EventsListViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sprite.screens.eventsList;
+
+import androidx.lifecycle.ViewModel;
+
+public class EventsListViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
