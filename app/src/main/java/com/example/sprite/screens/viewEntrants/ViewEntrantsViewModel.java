a954b285688c30c4fb6cf53303998c966b27bb49
NaimaU <naimaulusow42@gmail.com>
2025-10-29 22:42:57 -0600

    adding viewEntrants page UI

---
+++ app/src/main/java/com/example/sprite/screens/viewEntrants/ViewEntrantsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sprite.screens.viewEntrants;
+
+import androidx.lifecycle.ViewModel;
+
+public class ViewEntrantsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
